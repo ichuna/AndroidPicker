ad9305852710e69f34f0ce0c6e00e111f1b62b29
ichuna <ichuna@163.com>
2017-06-14 17:16:58 +0800

    Initial commit

---
+++ app/src/main/java/cn/qqtheme/androidpicker/MyPickerApp.kt
@@ -0,0 +1,44 @@
+package cn.qqtheme.androidpicker
+
+import android.app.Application
+
+import cn.qqtheme.framework.AppConfig
+import cn.qqtheme.framework.util.LogUtils
+
+/**
+ * ***********************************************
+ * **                ┏┓   ┏┓                    **
+ * **                ┏┛┻━━━┛┻┓                  **
+ * **                ┃       ┃                  **
+ * **                ┃   ━   ┃                  **
+ * **                ┃ ┳┛ ┗┳ ┃                  **
+ * **                ┃       ┃                  **
+ * **                ┃   ┻   ┃                  **
+ * **                ┃       ┃                  **
+ * **                ┗━┓   ┏━┛                  **
+ * **                  ┃   ┃                    **
+ * **                  ┃   ┃                    **
+ * **                  ┃   ┗━━━┓                **
+ * **                  ┃       ┣┓               **
+ * **                  ┃       ┏┛               **
+ * **                  ┗┓┓┏━┳┓┏┛                **
+ * **                   ┃┫┫ ┃┫┫                 **
+ * **                  ┗┻┛ ┗┻┛                  **
+ * ***********************************************
+ * **              神兽助我  扬我神威              **
+ * ***********************************************
+ * Author:李玉江[QQ:1032694760]
+ * DateTime:2016/7/20 20:28
+ * Builder:Android Studio
+ */
+class MyPickerApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        LogUtils.setIsDebug(BuildConfig.DEBUG)
+        if (!LogUtils.isDebug()) {
+            android.util.Log.d(AppConfig.DEBUG_TAG, "logcat is disabled")
+        }
+    }
+
+}
